10fa469ce2c4a42ec5dc498256ef223095f85d6f
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-01-26 23:06:50 +0530

    Template

---
+++ modular_exponentiation.cpp
@@ -0,0 +1,11 @@
+int powMod(int a, int b, int m = MOD) {
+    a %= m;
+    int res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
