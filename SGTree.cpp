ace2a7ba9121382ed184feb1185ebd5e00ae3468
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-05-14 23:41:35 +0530

    SGTree

--- SGTree.cpp
+++ SGTree.cpp
@@ -1,13 +1,13 @@
 class SGTree {
 public:
-	vector<int> segment;
+	vector<int> seg;
 	SGTree(int n) {
-		segment.resize(4 * n + 1);
+		seg.resize(4 * n + 1);
 	}
 
 	void build(int ind, int low, int high, vector <int> &arr) {
 		if (low == high) {
-			segment[ind] = arr[low];
+			seg[ind] = arr[low];
 			return;
 		}
 
@@ -15,12 +15,12 @@ public:
 		build(2 * ind + 1, low, mid, arr);
 		build(2 * ind + 2, mid + 1, high, arr);
 
-		segment[ind] = min(segment[2 * ind + 1], segment[2 * ind + 2]);
+		seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
 	}
 
 	int query(int ind, int low, int high, int l, int r) {
 		if (r < low || high < l) return mm;
-		if (low >= l && high <= r) return segment[ind];
+		if (low >= l && high <= r) return seg[ind];
 
 		int mid = low + (high - low) / 2;
 		int left = query(2 * ind + 1, low, mid, l, r);
@@ -31,7 +31,7 @@ public:
 
 	void update(int ind, int low, int high, int i, int val) {
 		if (low == high) {
-			segment[ind] = val;
+			seg[ind] = val;
 			return;
 		}
 
@@ -39,6 +39,6 @@ public:
 		if (i <= mid) update(2 * ind + 1, low, mid, i, val);
 		else update(2 * ind + 2, mid + 1, high, i, val);
 
-		segment[ind] = min(segment[2 * ind + 1], segment[2 * ind + 2]);
+		seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
 	}
 };
