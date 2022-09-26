b204597f2829bce115e8057c6d0f0b9a9b415f31
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-09-26 17:18:15 +0530

    DSU

--- dsu.cpp
+++ dsu.cpp
@@ -1,21 +1,29 @@
-const int n = 5 * 1e5;
-vector <int> parent(n), _size(n);
-void makeSet(int v) {
-	parent[v] = v;
-	_size[v] = 1;
-}
+class DSU {
+public:
+	vector <int> parent, _size;
 
-int findSet(int v) {
-	if (v == parent[v]) return v;
-	return parent[v] = findSet(parent[v]);
-}
-
-void unionSets(int a, int b) {
-	a = findSet(a);
-	b = findSet(b);
-	if (a != b) {
-		if (_size[a] < _size[b]) swap(a, b);
-		parent[b] = a;
-		_size[a] += _size[b];
+	DSU (int n) {
+		parent.resize(n);
+		_size.resize(n);
 	}
-}
+
+	void makeSet(int v) {
+		parent[v] = v;
+		_size[v] = 1;
+	}
+
+	int findSet(int v) {
+		if (v == parent[v]) return v;
+		return parent[v] = findSet(parent[v]);
+	}
+
+	void unionSets(int a, int b) {
+		a = findSet(a);
+		b = findSet(b);
+		if (a != b) {
+			if (_size[a] < _size[b]) swap(a, b);
+			parent[b] = a;
+			_size[a] += _size[b];
+		}
+	}
+};
