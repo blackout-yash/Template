e0e3e90fc8a91c2788e716d446f5b6c4fc04ebc8
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-11-15 00:58:33 +0530

    Template

--- Template/debug.h
+++ Template/debug.h
@@ -4,21 +4,31 @@
 #include <queue>
 #include <vector>
 
-using namespace std;
-#define int long long
+// #include<ext/pb_ds/tree_policy.hpp>
+// #include<ext/pb_ds/assoc_container.hpp>
 
-#define debug(x) cout << #x << " => "; pprintt(x); cout << "\n";
-template <typename T> void pprintt(T t) {cout << t;}
+#define int long long
+using namespace std;
+// using namespace __gnu_pbds;
+
+// typedef tree <int, null_type, less <int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// typedef tree <pair <int, int>, null_type, less <pair <int, int>>, rb_tree_tag, tree_order_statistics_node_update> multipbds;
+
+#define debug(x) cerr << #x << " => "; pprintt(x); cerr << "\n";
+template <typename T> void pprintt(T t) {cerr << t;}
 template <class T, class V> void pprintt(pair <T, V> p);
 template <class T> void pprintt(vector <T> v);
 template <class T> void pprintt(set <T> v);
 template <class T, class V> void pprintt(map <T, V> v);
 template <class T> void pprintt(multiset <T> v);
-template <class T, class V> void pprintt(pair <T, V> p) {cout << "{"; pprintt(p.first); cout << ","; pprintt(p.second); cout << "}";}
-template <class T> void pprintt(vector <T> v) {cout << "[ "; for (T i : v) {pprintt(i); cout << " ";} cout << "]\n";}
-template <class T> void pprintt(set <T> v) {cout << "[ "; for (T i : v) {pprintt(i); cout << " ";} cout << "]";}
-template <class T> void pprintt(queue <T> v) {cout << "[ "; while (v.size()) {pprintt(v.front()); cout << " "; v.pop();} cout << "]";}
-template <class T> void pprintt(priority_queue <T> v) {cout << "[ "; while (v.size()) {pprintt(v.top()); cout << " "; v.pop();} cout << "]";}
-template <class T> void pprintt(stack <T> v) {cout << "[ "; while (v.size()) {pprintt(v.top()); cout << " "; v.pop();} cout << "]";}
-template <class T> void pprintt(multiset <T> v) {cout << "[ "; for (T i : v) {pprintt(i); cout << " ";} cout << "]";}
-template <class T, class V> void pprintt(map <T, V> v) {cout << "[ "; for (auto i : v) {pprintt(i); cout << " ";} cout << "]";}
+template <class T, class V> void pprintt(pair <T, V> p) {cerr << "{"; pprintt(p.first); cerr << ","; pprintt(p.second); cerr << "}";}
+template <class T> void pprintt(vector <T> v) {cerr << "[ "; for (T i : v) {pprintt(i); cerr << " ";} cerr << "]\n";}
+template <class T> void pprintt(set <T> v) {cerr << "[ "; for (T i : v) {pprintt(i); cerr << " ";} cerr << "]";}
+template <class T> void pprintt(queue <T> v) {cerr << "[ "; while (v.size()) {pprintt(v.front()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void pprintt(priority_queue <T> v) {cerr << "[ "; while (v.size()) {pprintt(v.top()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void pprintt(stack <T> v) {cerr << "[ "; while (v.size()) {pprintt(v.top()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void pprintt(multiset <T> v) {cerr << "[ "; for (T i : v) {pprintt(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void pprintt(map <T, V> v) {cerr << "[ "; for (auto i : v) {pprintt(i); cerr << " ";} cerr << "]";}
+
+// void pprintt(pbds v) {cerr << "[ "; for (auto i : v) {pprintt(i); cerr << " ";} cerr << "]";}
+// void pprintt(multipbds v) {cerr << "[ "; for (auto i : v) {pprintt(i); cerr << " ";} cerr << "]";}
