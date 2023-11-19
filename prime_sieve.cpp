8af85ae1158799e14275d6d2beba77801bf577e4
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-11-19 12:57:46 +0530

    Prime

--- prime_sieve.cpp
+++ prime_sieve.cpp
@@ -1,18 +1,17 @@
+#include <bitset>
+#define int long long
+
+vector <int> prime;
 const int range = 1e7;
 bitset < range + 5 > b;
-vector <int> prime;
 
 void sieve() {
     b.set();
-
     b[0] = b[1] = 0;
-
     for (int i = 2; i <= range; i++) {
         if (b[i]) {
             prime.push_back(i);
-            for (int j = i * i; j <= range; j += i) {
-                b[j] = 0;
-            }
+            for (int j = i * i; j <= range; j += i) b[j] = 0;
         }
     }
 }
