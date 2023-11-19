c50889120290964b186721515c46e4cd10d4e027
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-11-19 12:55:26 +0530

    Factor

--- Factor/prime_factor.cpp
+++ Factor/prime_factor.cpp
@@ -1,16 +1,11 @@
 vector <int> primeFactors(int n) {
 	vector <int> factor;
-    while (n % 2 == 0) {  
-        factor.push_back(2);  
-        n = n / 2;  
-    } 
-	for (int i = 1; i <= sqrt(n); i++) {
+	for (int i = 2; i * i <= n; i++) {
 		while (n % i == 0) {  
-            factor.push_back(i);  
-            n = n / i;  
-        } 
+            		factor.push_back(i);  
+            		n = n / i;  
+		} 
 	} 
-    if (n > 2) factor.push_back(n);
-
+    if (n >= 2) factor.push_back(n);
 	return factor;
 }
