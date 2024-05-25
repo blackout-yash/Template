ed2cd79f4a402598b79ec6d6391a6dab43cdb638
Yash Kumar <yashkumarjmp2002@gmail.com>
2024-05-25 15:10:07 +0530

    Pi Function

---
+++ String/pi_function.cpp
@@ -0,0 +1,29 @@
+vector <int> compute_pi(string &str) {
+  int n = str.size();
+  vector <int> pi(n);
+  for (int i = 1; i < n; i++) {
+    int j = pi[i - 1]; 
+    while (j > 0 && str[j] != str[i]) j = pi[j - 1];
+    if (str[j] == str[i]) pi[i] = j + 1;
+  }
+  return pi;
+}
+ 
+vector <int> allOccurence(string &pat, string &text) {
+    string s = pat + "$" + text;
+    int n = pat.size(), m = text.size();
+    vector <int> pi = compute_pi(s), ans;
+    for (int i = n + 1; i <= n + m; i++) {
+        if (pi[i] == n) {
+            int curr = i - 2 * n;
+            // Only non overlapping occurence
+            if(ans.empty()) ans.push_back(curr);
+            else if(curr - ans.back() >= k) ans.push_back(curr);
+
+            // all occurence
+            // ans.push_back(curr);
+        }
+    }
+ 
+    return ans; 
+}
